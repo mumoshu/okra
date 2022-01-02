8a6e4a806fbf794cedc85232e597ccb843b071df
Yusuke Kuoka <ykuoka@gmail.com>
2022-01-02 02:25:00 +0000

    refactor: Extract reconcilation of experiment out of main cell sync logic

--- pkg/cell/analysis.go
+++ pkg/cell/analysis.go
@@ -8,6 +8,8 @@ import (
 
 	rolloutsv1alpha1 "github.com/mumoshu/okra/api/rollouts/v1alpha1"
 	okrav1alpha1 "github.com/mumoshu/okra/api/v1alpha1"
+	"github.com/mumoshu/okra/pkg/sync"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -136,6 +138,173 @@ func (s cellComponentReconciler) reconcileAnalysisRun(ctx context.Context, compo
 		}
 	default:
 		return ComponentInProgress, errors.New("too many analysis runs")
+	}
+
+	return ComponentPassed, nil
+}
+
+func (s cellComponentReconciler) reconcileExperiment(ctx context.Context, componentID string, exTemplate *rolloutsv1alpha1.RolloutExperimentStep) (componentReconcilationResult, error) {
+	//
+	// Ensure that the previous experiments has been successful, if any
+	//
+
+	runtimeClient := s.runtimeClient
+	scheme := s.scheme
+	cell := s.cell
+
+	var experimentList rolloutsv1alpha1.ExperimentList
+
+	labelSelector, err := labels.Parse(LabelKeyStepIndex + "=" + componentID)
+	if err != nil {
+		return ComponentInProgress, err
+	}
+
+	if err := runtimeClient.List(ctx, &experimentList, &client.ListOptions{
+		LabelSelector: labelSelector,
+	}); err != nil {
+		return ComponentInProgress, err
+	}
+
+	numExperiments := len(experimentList.Items)
+
+	var ex rolloutsv1alpha1.Experiment
+	{
+
+		d := exTemplate.Duration
+
+		var templates []rolloutsv1alpha1.TemplateSpec
+
+		for _, t := range exTemplate.Templates {
+			var rs appsv1.ReplicaSet
+			nsName := types.NamespacedName{Namespace: cell.Namespace, Name: string(t.SpecRef)}
+			if err := runtimeClient.Get(ctx, nsName, &rs); err != nil {
+				log.Printf("Failed getting experiment template replicaset %s: %v", nsName, err)
+				return ComponentInProgress, err
+			}
+
+			s := t.Selector
+			if s == nil {
+				s = rs.Spec.Selector
+			}
+
+			templates = append(templates, rolloutsv1alpha1.TemplateSpec{
+				Name:     t.Name,
+				Replicas: t.Replicas,
+				Selector: s,
+				Template: rs.Spec.Template,
+			})
+		}
+
+		var analyses []rolloutsv1alpha1.ExperimentAnalysisTemplateRef
+		for _, a := range exTemplate.Analyses {
+			var args []rolloutsv1alpha1.Argument
+			for _, arg := range a.Args {
+				args = append(args, rolloutsv1alpha1.Argument{
+					Name: arg.Name,
+					// TODO
+					Value: &arg.Value,
+				})
+			}
+			analyses = append(analyses, rolloutsv1alpha1.ExperimentAnalysisTemplateRef{
+				Name:                  a.Name,
+				TemplateName:          a.TemplateName,
+				Args:                  args,
+				RequiredForCompletion: a.RequiredForCompletion,
+			})
+		}
+
+		spec := rolloutsv1alpha1.ExperimentSpec{
+			Duration:  d,
+			Templates: templates,
+			Analyses:  analyses,
+		}
+
+		templateHash := sync.ComputeHash(spec)
+
+		ex = rolloutsv1alpha1.Experiment{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: cell.Namespace,
+				Name:      fmt.Sprintf("%s-%d-%s", cell.Name, componentID, "experiment"),
+				Labels: map[string]string{
+					LabelKeyStepIndex:    componentID,
+					LabelKeyCell:         cell.Name,
+					LabelKeyTemplateHash: templateHash,
+				},
+			},
+			Spec: spec,
+		}
+		if err := ctrl.SetControllerReference(&cell, &ex, scheme); err != nil {
+			log.Printf("Failed setting controller reference on %s/%s: %v", ex.Namespace, ex.Name, err)
+		}
+	}
+
+	if numExperiments == 0 {
+		if err := runtimeClient.Create(ctx, &ex); err != nil {
+			return ComponentInProgress, err
+		}
+
+		log.Printf("Created experiment %s", ex.Name)
+
+		return ComponentInProgress, nil
+	}
+
+	if numExperiments > 1 {
+		return ComponentInProgress, errors.New("too many experiments")
+	}
+
+	var currentTemplateHash string
+	if annotations := experimentList.Items[0].GetAnnotations(); annotations != nil {
+		if templateHash := annotations[LabelKeyTemplateHash]; templateHash != "" {
+			currentTemplateHash = templateHash
+		}
+	}
+
+	var desiredTemplateHash string
+	if annotations := ex.GetAnnotations(); annotations != nil {
+		if templateHash := annotations[LabelKeyTemplateHash]; templateHash != "" {
+			desiredTemplateHash = templateHash
+		}
+	}
+
+	if currentTemplateHash != desiredTemplateHash {
+		var current rolloutsv1alpha1.Experiment
+
+		if err := runtimeClient.Get(ctx, types.NamespacedName{Namespace: ex.Namespace, Name: ex.Name}, &current); err != nil {
+			return ComponentInProgress, err
+		}
+
+		current.Spec = ex.Spec
+
+		for k, v := range ex.Labels {
+			current.Labels[k] = v
+		}
+
+		for k, v := range ex.Annotations {
+			current.Annotations[k] = v
+		}
+
+		if err := runtimeClient.Update(ctx, &current); err != nil {
+			return ComponentInProgress, err
+		}
+
+		log.Printf("Updated experiment %s", ex.Name)
+
+		return ComponentInProgress, nil
+	}
+
+	unchangedEx := experimentList.Items[0]
+
+	switch unchangedEx.Status.Phase {
+	case rolloutsv1alpha1.AnalysisPhaseSuccessful:
+	case rolloutsv1alpha1.AnalysisPhaseError, rolloutsv1alpha1.AnalysisPhaseFailed:
+		log.Printf("Experiment %s failed with error: %v", ex.Name, ex.Status.Message)
+
+		return ComponentFailed, nil
+	default:
+		log.Printf("Waiting for experiment %s of %s to become %s", ex.Name, ex.Status.Phase, rolloutsv1alpha1.AnalysisPhaseSuccessful)
+
+		// We need to wait for this analysis run to succeed
+		return ComponentInProgress, nil
 	}
 
 	return ComponentPassed, nil
--- pkg/cell/cell.go
+++ pkg/cell/cell.go
@@ -2,7 +2,6 @@ package cell
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
 	"strconv"
@@ -14,7 +13,6 @@ import (
 	"github.com/mumoshu/okra/pkg/awstargetgroupset"
 	"github.com/mumoshu/okra/pkg/clclient"
 	"github.com/mumoshu/okra/pkg/sync"
-	appsv1 "k8s.io/api/apps/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -434,177 +432,15 @@ func Sync(config SyncInput) error {
 						break STEPS
 					}
 				} else if step.Experiment != nil {
-					//
-					// Ensure that the previous experiments has been successful, if any
-					//
-
-					var experimentList rolloutsv1alpha1.ExperimentList
-
-					labelSelector, err := labels.Parse(LabelKeyStepIndex + "=" + stepIndexStr)
+					r, err := ccr.reconcileExperiment(ctx, stepIndexStr, step.Experiment)
 					if err != nil {
 						return err
-					}
-
-					if err := runtimeClient.List(ctx, &experimentList, &client.ListOptions{
-						LabelSelector: labelSelector,
-					}); err != nil {
-						return err
-					}
-
-					numExperiments := len(experimentList.Items)
-
-					var ex rolloutsv1alpha1.Experiment
-					{
-						exTemplate := step.Experiment
-
-						d := exTemplate.Duration
-
-						var templates []rolloutsv1alpha1.TemplateSpec
-
-						for _, t := range exTemplate.Templates {
-							var rs appsv1.ReplicaSet
-							nsName := types.NamespacedName{Namespace: cell.Namespace, Name: string(t.SpecRef)}
-							if err := runtimeClient.Get(ctx, nsName, &rs); err != nil {
-								log.Printf("Failed getting experiment template replicaset %s: %v", nsName, err)
-								return err
-							}
-
-							s := t.Selector
-							if s == nil {
-								s = rs.Spec.Selector
-							}
-
-							templates = append(templates, rolloutsv1alpha1.TemplateSpec{
-								Name:     t.Name,
-								Replicas: t.Replicas,
-								Selector: s,
-								Template: rs.Spec.Template,
-							})
-						}
-
-						var analyses []rolloutsv1alpha1.ExperimentAnalysisTemplateRef
-						for _, a := range exTemplate.Analyses {
-							var args []rolloutsv1alpha1.Argument
-							for _, arg := range a.Args {
-								args = append(args, rolloutsv1alpha1.Argument{
-									Name: arg.Name,
-									// TODO
-									Value: &arg.Value,
-								})
-							}
-							analyses = append(analyses, rolloutsv1alpha1.ExperimentAnalysisTemplateRef{
-								Name:                  a.Name,
-								TemplateName:          a.TemplateName,
-								Args:                  args,
-								RequiredForCompletion: a.RequiredForCompletion,
-							})
-						}
-
-						spec := rolloutsv1alpha1.ExperimentSpec{
-							Duration:  d,
-							Templates: templates,
-							Analyses:  analyses,
-						}
-
-						templateHash := sync.ComputeHash(spec)
-
-						ex = rolloutsv1alpha1.Experiment{
-							ObjectMeta: metav1.ObjectMeta{
-								Namespace: cell.Namespace,
-								Name:      fmt.Sprintf("%s-%d-%s", cell.Name, stepIndex, "experiment"),
-								Labels: map[string]string{
-									LabelKeyStepIndex:    stepIndexStr,
-									LabelKeyCell:         cell.Name,
-									LabelKeyTemplateHash: templateHash,
-								},
-							},
-							Spec: spec,
-						}
-						if err := ctrl.SetControllerReference(&cell, &ex, scheme); err != nil {
-							log.Printf("Failed setting controller reference on %s/%s: %v", ex.Namespace, ex.Name, err)
-						}
-					}
-
-					if numExperiments == 0 {
-						if err := runtimeClient.Create(ctx, &ex); err != nil {
-							return err
-						}
-
-						log.Printf("Created experiment %s", ex.Name)
-
+					} else if r == ComponentInProgress {
+						break STEPS
+					} else if r == ComponentFailed {
+						anyStepFailed = true
 						break STEPS
 					}
-
-					if numExperiments > 1 {
-						return errors.New("too many experiments")
-					}
-
-					var currentTemplateHash string
-					if annotations := experimentList.Items[0].GetAnnotations(); annotations != nil {
-						if templateHash := annotations[LabelKeyTemplateHash]; templateHash != "" {
-							currentTemplateHash = templateHash
-						}
-					}
-
-					var desiredTemplateHash string
-					if annotations := ex.GetAnnotations(); annotations != nil {
-						if templateHash := annotations[LabelKeyTemplateHash]; templateHash != "" {
-							desiredTemplateHash = templateHash
-						}
-					}
-
-					if currentTemplateHash != desiredTemplateHash {
-						var current rolloutsv1alpha1.Experiment
-
-						if err := runtimeClient.Get(ctx, types.NamespacedName{Namespace: ex.Namespace, Name: ex.Name}, &current); err != nil {
-							return err
-						}
-
-						current.Spec = ex.Spec
-
-						for k, v := range ex.Labels {
-							current.Labels[k] = v
-						}
-
-						for k, v := range ex.Annotations {
-							current.Annotations[k] = v
-						}
-
-						if err := runtimeClient.Update(ctx, &current); err != nil {
-							return err
-						}
-
-						log.Printf("Updated experiment %s", ex.Name)
-
-						break STEPS
-					}
-
-					if numExperiments == 1 {
-						for _, ex := range experimentList.Items {
-							if ex.Status.Phase == rolloutsv1alpha1.AnalysisPhaseError {
-								log.Printf("Experiment %s failed with error: %v", ex.Name, ex.Status.Message)
-
-								anyStepFailed = true
-								break STEPS
-							}
-
-							if ex.Status.Phase != rolloutsv1alpha1.AnalysisPhaseSuccessful {
-								if ex.Status.Phase == rolloutsv1alpha1.AnalysisPhaseFailed {
-									// TODO Suspend and mark it as permanent failure when experiment timed out
-									log.Printf("Experiment %s failed", ex.Name)
-
-									anyStepFailed = true
-									break STEPS
-								}
-
-								log.Printf("Waiting for experiment %s of %s to become %s", ex.Name, ex.Status.Phase, rolloutsv1alpha1.AnalysisPhaseSuccessful)
-
-								// We need to wait for this analysis run to succeed
-								break STEPS
-							}
-						}
-					}
-
 				} else if step.SetWeight != nil {
 					desiredStableTGsWeight -= int(*step.SetWeight)
 
